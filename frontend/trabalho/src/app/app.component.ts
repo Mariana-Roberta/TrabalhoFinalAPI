436ae8daf1d8b6bc886a455df98847cc2638d477
Mariana-Roberta <marianaroberta.240@gmail.com>
2025-04-02 13:10:07 -0300

    Criando outro frontend

--- frontend/trabalho/src/app/app.component.ts
+++ frontend/trabalho/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabalho-final';
+  title = 'trabalho';
 }
